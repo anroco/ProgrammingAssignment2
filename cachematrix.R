a1a6c20a859b213ebb7ab159fc2b4c78e8f3e764
anroco <anrocox@gmail.com>
2015-02-19 20:36:26 -0500

    add comments to functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,6 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## This function allows you to create a special matrix, returns a list of
+## functions getMatrix and setMatrix, also returns the functions getInvMatrix
+## and setInvMatrix which allow you to manage the inverted matrix.
 makeCacheMatrix <- function(x = matrix()) {
   invMatrix <- NULL
   setMatrix <- function(matrix) {
@@ -17,9 +15,10 @@ makeCacheMatrix <- function(x = matrix()) {
        getInvMatrix = getInvMatrix)
 }
 
-
-## Write a short comment describing this function
-
+## This function allows to get the inverse matrix of the matrix created with
+## the function makeCacheMatrix. If the inverse matrix has not been calculated
+## bove will be calculated and returned. If the inverse matrix has been 
+## calculated above will be returned.
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   inv_matrix <- x$getInvMatrix()
